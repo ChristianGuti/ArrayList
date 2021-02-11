af5ccfa87d70b14a4c17c3c6ae2e6c9309588fed
christian gutierrez <alugut445@ieselcaminas.org>
2021-02-11 20:09:29 +0100

    Initial commit

---
+++ src/ArrayList0.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ArrayList0 {
+    public static void main(String[] args) {
+
+        ArrayList lista = new ArrayList();
+
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+        lista.add(-211);
+
+        System.out.println("La posición 0 contiene un ");
+    }
+}
